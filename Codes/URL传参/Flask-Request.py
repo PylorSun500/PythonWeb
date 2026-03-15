65331b5180398f7c711c2490952db795f207849d
PylorSun <pylorsun500@outlook.com>
2026-03-15 21:48:34 +0800

    整理了请求和重定向相关知识点 并整理了gitignore，正在研究怎么把 main 中的已有但之前提交时没有 ignore 的内容给删了。除非...这个提交就是？

--- Codes/URL传参/Flask-Request.py
+++
@@ -1,8 +0,0 @@
-# 使用 request.args.get() 控制从 URL 传入的参数。
-from flask import Flask, request
-app = Flask(__name__)
-
-@app.route("/gettest")
-def gettest():
-    return "你的姓名为{}，你的年龄为{}".format(request.args.get("name"), \
-                                           request.args.get("age"))
